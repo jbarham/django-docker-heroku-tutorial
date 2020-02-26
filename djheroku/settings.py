efa2d928b22b6025198b444754abd8dbf73bfda6
John Barham <jbarham@gmail.com>
2020-02-26 21:30:54 +1100

    Use WhiteNoise in development

--- djheroku/settings.py
+++ djheroku/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django_rq',
     'debug_toolbar',
