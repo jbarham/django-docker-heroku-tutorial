93765bebd7b5f2a24921470e3518185dcf017a55
John Barham <jbarham@gmail.com>
2022-01-08 22:14:25 +1100

    Don't use BigAutoField for DEFAULT_AUTO_FIELD (quiet migration warning)

--- djheroku/settings.py
+++ djheroku/settings.py
@@ -163,4 +163,4 @@ DEBUG_TOOLBAR_CONFIG = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
