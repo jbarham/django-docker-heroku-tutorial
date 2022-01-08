d9faeddb4eb3b2fde3305db1528081ee615c63cb
John Barham <jbarham@gmail.com>
2022-01-08 21:48:03 +1100

    Formatting

--- keygen/models.py
+++ keygen/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.management.utils import get_random_secret_key
 
+
 class Secret(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     key = models.CharField(max_length=50, default=get_random_secret_key)
--- keygen/views.py
+++ keygen/views.py
@@ -7,15 +7,18 @@ from django_rq import job
 
 from .models import Secret
 
+
 # https://github.com/rq/django-rq#job-decorator
 @job
 def generate_bg():
-    time.sleep(2) # Simulate expensive operation.
+    time.sleep(2)  # Simulate expensive operation.
     Secret.objects.create()
+
 
 def index(request):
     context = {'secrets': Secret.objects.all()}
     return render(request, 'keygen/index.html', context)
+
 
 def generate(request):
     if request.GET.get('bg'):
@@ -25,6 +28,7 @@ def generate(request):
         Secret.objects.create()
         messages.success(request, 'Generated new key.')
     return redirect('home')
+
 
 def delete(request):
     Secret.objects.all().delete()
